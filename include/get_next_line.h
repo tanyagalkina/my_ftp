053e44ffb395d7d99aba2a3726035c854bfd703a
tatjana.galkin <tatjana.galkin@epitech.eu>
2021-04-22 15:13:49 +0200

    *.h

---
+++ include/get_next_line.h
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2019
+** GetNextLine
+** File description:
+** GetNextLine.h
+*/
+
+#ifndef GETNEXTLINE_H_
+#define GETNEXTLINE_H_
+
+#ifndef READ_SIZE
+#define READ_SIZE (128)
+
+char *get_next_line(int fd);
+
+#endif
+/*READ_SIZE*/
+#endif
+/*GETNEXTLINE_H_ */
+
